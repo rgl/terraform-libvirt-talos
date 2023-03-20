898ab011e153131e7ea9d69d6f7b229e9d6fd43d
Rui Lopes <rgl@ruilopes.com>
2023-03-20 22:19:00 +0000

    upgrade to kubernetes 1.26.3

--- main.tf
+++ main.tf
@@ -64,7 +64,7 @@ variable "cluster_name" {
 
 locals {
   talos_version      = "1.3.6"
-  kubernetes_version = "1.26.2"
+  kubernetes_version = "1.26.3"
   cluster_vip        = "10.17.3.9"
   cluster_endpoint   = "https://${local.cluster_vip}:6443" # k8s api-server endpoint.
   controller_nodes = [
