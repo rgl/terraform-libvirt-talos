9a153f610c3b63ee8ec017bedb40bd7943a229a2
Rui Lopes <rgl@ruilopes.com>
2023-04-06 18:28:12 +0100

    upgrade to talos 1.3.7

--- main.tf
+++ main.tf
@@ -63,7 +63,7 @@ variable "cluster_name" {
 }
 
 locals {
-  talos_version      = "1.3.6"
+  talos_version      = "1.3.7"
   kubernetes_version = "1.26.3"
   cluster_vip        = "10.17.3.9"
   cluster_endpoint   = "https://${local.cluster_vip}:6443" # k8s api-server endpoint.
