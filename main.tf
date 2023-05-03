16959432edb057d98ab5f7ddd7ae27c51820c955
Rui Lopes <rgl@ruilopes.com>
2023-05-03 12:13:36 +0100

    upgrade to kubernetes 1.26.4

--- main.tf
+++ main.tf
@@ -63,7 +63,7 @@ variable "cluster_name" {
 }
 
 locals {
-  kubernetes_version = "1.26.3"
+  kubernetes_version = "1.26.4" # see https://github.com/siderolabs/kubelet/pkgs/container/kubelet
   talos_version      = "1.4.1"  # see https://github.com/siderolabs/talos/releases
   talos_version_tag  = "v${local.talos_version}"
   cluster_vip        = "10.17.3.9"
