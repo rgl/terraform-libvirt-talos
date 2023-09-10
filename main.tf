6e448af4f7d4323f85d715590865ab872331f8de
Rui Lopes <rgl@ruilopes.com>
2023-09-10 10:07:52 +0100

    upgrade to qemu-guest-agent extension 8.1.0

--- main.tf
+++ main.tf
@@ -63,7 +63,7 @@ variable "cluster_name" {
 }
 
 locals {
-  qemu_guest_agent_extension_version = "8.0.2"  # see https://github.com/siderolabs/extensions/pkgs/container/qemu-guest-agent
+  qemu_guest_agent_extension_version = "8.1.0"  # see https://github.com/siderolabs/extensions/pkgs/container/qemu-guest-agent
   kubernetes_version                 = "1.26.8" # see https://github.com/siderolabs/kubelet/pkgs/container/kubelet
   talos_version                      = "1.5.2"  # see https://github.com/siderolabs/talos/releases
   talos_version_tag                  = "v${local.talos_version}"
