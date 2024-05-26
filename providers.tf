ef16982a80af61ac882e937ef82959d9ddff1c25
Rui Lopes <rgl@ruilopes.com>
2024-05-26 18:43:42 +0100

    upgrade to hashicorp/random 3.6.1

--- providers.tf
+++ providers.tf
@@ -6,7 +6,7 @@ terraform {
     # see https://github.com/hashicorp/terraform-provider-random
     random = {
       source  = "hashicorp/random"
-      version = "3.6.1"
+      version = "3.6.2"
     }
     # see https://registry.terraform.io/providers/dmacvicar/libvirt
     # see https://github.com/dmacvicar/terraform-provider-libvirt
