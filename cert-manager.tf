28dbb41cec0a31985165596ff6e01577a5919147
Rui Lopes <rgl@ruilopes.com>
2024-08-12 06:40:15 +0100

    upgrade to cert-manager 1.15.2

--- cert-manager.tf
+++ cert-manager.tf
@@ -73,7 +73,7 @@ data "helm_template" "cert_manager" {
   repository = "https://charts.jetstack.io"
   chart      = "cert-manager"
   # renovate: datasource=helm depName=cert-manager registryUrl=https://charts.jetstack.io
-  version      = "1.15.1"
+  version      = "1.15.2"
   kube_version = var.kubernetes_version
   api_versions = []
   # NB installCRDs is generally not recommended, BUT since this
