43d0a5e9bb5c09872bca6fd0cc8e813e4338b0d2
Rui Lopes <rgl@ruilopes.com>
2024-10-12 15:43:20 +0100

    upgrade to hashicorp/helm 2.16.0

--- providers.tf
+++ providers.tf
@@ -24,7 +24,7 @@ terraform {
     # see https://github.com/hashicorp/terraform-provider-helm
     helm = {
       source  = "hashicorp/helm"
-      version = "2.15.0"
+      version = "2.16.0"
     }
     # see https://registry.terraform.io/providers/rgl/kustomizer
     # see https://github.com/rgl/terraform-provider-kustomizer
