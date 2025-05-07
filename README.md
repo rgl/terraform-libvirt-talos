00e42b43d1869687af40be33a5e5093d1356a95a
Rui Lopes <rgl@ruilopes.com>
2025-05-07 20:51:31 +0100

    upgrade to talos 1.10.1

--- README.md
+++ README.md
@@ -89,7 +89,7 @@ Install talosctl:
 ```bash
 # see https://github.com/siderolabs/talos/releases
 # renovate: datasource=github-releases depName=siderolabs/talos
-talos_version='1.9.5'
+talos_version='1.10.1'
 wget https://github.com/siderolabs/talos/releases/download/v$talos_version/talosctl-linux-amd64
 sudo install talosctl-linux-amd64 /usr/local/bin/talosctl
 rm talosctl-linux-amd64
@@ -506,7 +506,7 @@ Update the talos extensions to match the talos version:
 Talos:
 
 ```bash
-# see https://www.talos.dev/v1.9/advanced/troubleshooting-control-plane/
+# see https://www.talos.dev/v1.10/advanced/troubleshooting-control-plane/
 talosctl -n $all support && rm -rf support && 7z x -osupport support.zip && code support
 talosctl -n $c0 service ext-qemu-guest-agent status
 talosctl -n $c0 service etcd status
--- talos.tf
+++ talos.tf
@@ -18,7 +18,7 @@ locals {
       #    talos image, which is created in the installed state.
       #install = {}
       features = {
-        # see https://www.talos.dev/v1.9/kubernetes-guides/configuration/kubeprism/
+        # see https://www.talos.dev/v1.10/kubernetes-guides/configuration/kubeprism/
         # see talosctl -n $c0 read /etc/kubernetes/kubeconfig-kubelet | yq .clusters[].cluster.server
         # NB if you use a non-default CNI, you must configure it to use the
         #    https://localhost:7445 kube-apiserver endpoint.
@@ -26,7 +26,7 @@ locals {
           enabled = true
           port    = 7445
         }
-        # see https://www.talos.dev/v1.9/talos-guides/network/host-dns/
+        # see https://www.talos.dev/v1.10/talos-guides/network/host-dns/
         hostDNS = {
           enabled              = true
           forwardKubeDNSToHost = true
@@ -76,8 +76,8 @@ locals {
       }
     }
     cluster = {
-      # see https://www.talos.dev/v1.9/talos-guides/discovery/
-      # see https://www.talos.dev/v1.9/reference/configuration/#clusterdiscoveryconfig
+      # see https://www.talos.dev/v1.10/talos-guides/discovery/
+      # see https://www.talos.dev/v1.10/reference/configuration/#clusterdiscoveryconfig
       discovery = {
         enabled = true
         registries = {
@@ -122,7 +122,7 @@ data "talos_machine_configuration" "controller" {
       machine = {
         network = {
           interfaces = [
-            # see https://www.talos.dev/v1.9/talos-guides/network/vip/
+            # see https://www.talos.dev/v1.10/talos-guides/network/vip/
             {
               interface = "eth0"
               dhcp      = true
