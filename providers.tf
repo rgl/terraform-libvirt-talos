4358ab1f648cab97373656d0443700bb2a380b52
Rui Lopes <rgl@ruilopes.com>
2025-06-13 18:25:51 +0000

    upgrade to terraform 1.12.2

--- providers.tf
+++ providers.tf
@@ -1,6 +1,6 @@
 # see https://github.com/hashicorp/terraform
 terraform {
-  required_version = "1.12.1"
+  required_version = "1.12.2"
   required_providers {
     # see https://registry.terraform.io/providers/hashicorp/random
     # see https://github.com/hashicorp/terraform-provider-random
