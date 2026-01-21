7cfdf917de6ac4a38dbc4072eed1b9c03de612b5
Rui Lopes <rgl@ruilopes.com>
2026-01-21 22:00:22 +0000

    upgrade to kubernetes 1.35.0

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@ variable "talos_version" {
 variable "kubernetes_version" {
   type = string
   # renovate: datasource=github-releases depName=siderolabs/kubelet
-  default = "1.34.3"
+  default = "1.35.0"
   validation {
     condition     = can(regex("^\\d+(\\.\\d+)+", var.kubernetes_version))
     error_message = "Must be a version number."
