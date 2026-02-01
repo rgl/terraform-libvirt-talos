face52378fd312e0e98c43cd61ceb812b3fe1f88
Rui Lopes <rgl@ruilopes.com>
2026-02-01 15:20:00 +0000

    upgrade to terraform 1.14.4

--- providers.tf
+++ providers.tf
@@ -1,6 +1,6 @@
 # see https://github.com/hashicorp/terraform
 terraform {
-  required_version = "1.14.3"
+  required_version = "1.14.4"
   required_providers {
     # see https://registry.terraform.io/providers/hashicorp/random
     # see https://github.com/hashicorp/terraform-provider-random
